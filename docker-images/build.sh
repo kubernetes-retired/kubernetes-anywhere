9c3d08d3b0349fe5f3db4f55a130fbed145a969f
Ilya Dmitrichenko <errordeveloper@gmail.com>
2015-11-04 14:14:51 -0800

    Assume local docker

--- docker-images/build.sh
+++ docker-images/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh -x
 for i in kubelet proxy apiserver controller-manager scheduler tools
-do docker $(docker-machine config kubedev) build -t weaveworks/kubernetes-anywhere:$i -f ./$i ./
+do docker build -t weaveworks/kubernetes-anywhere:$i -f ./$i ./
 done
