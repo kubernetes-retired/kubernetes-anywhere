2ddac2a2afdfc93e8a4e8f3208e8839ed8846317
Ilya Dmitrichenko <errordeveloper@gmail.com>
2015-11-04 15:11:53 -0800

    Should pass the socker path

--- launch-all.sh
+++ launch-all.sh
@@ -2,5 +2,5 @@
 ./weave launch-proxy --rewrite-inspect
 ./weave expose -h $HOSTNAME.weave.local
 
-docker run weaveworks/kubernetes-anywhere:tools compose up
+docker run -v /var/run/weave/weave.sock:/weave.sock weaveworks/kubernetes-anywhere:tools compose up
 docker run weaveworks/kubernetes-anywhere:tools kubectl create -f /skydns-addon
