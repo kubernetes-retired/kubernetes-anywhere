81a776e7186aabf15258bea8f773620413abbe7a
Ilya Dmitrichenko <errordeveloper@gmail.com>
2015-11-05 15:06:21 -0800

    Add etcd to the list of things to build

--- docker-images/build.sh
+++ docker-images/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh -x
-for i in kubelet proxy apiserver controller-manager scheduler tools
+for i in etcd kubelet proxy apiserver controller-manager scheduler tools
 do docker build -t weaveworks/kubernetes-anywhere:$i -f ./$i ./
 done
