afc51713daf644f5fc2ecb70393ad26dc2a9a85a
Ilya Dmitrichenko <errordeveloper@gmail.com>
2016-01-06 18:11:11 +0000

    Pass config file as we have it now

--- docker-images/controller-manager-anywhere.sh
+++ docker-images/controller-manager-anywhere.sh
@@ -6,7 +6,7 @@ master="kube-apiserver.weave.local"
 
 if [ -d $config ]
 then
-  args="--master=https://${master}:6443 --service-account-private-key-file=${config}/kube-apiserver.key --root-ca-file=${config}/kube-ca.crt"
+  args="--kubeconfig=${config}/kubeconfig --service-account-private-key-file=${config}/kube-apiserver.key --root-ca-file=${config}/kube-ca.crt"
 else
   args="--master=http://${master}:8080"
 fi
