kubernetes-retired/kubernetes-anywhere

commit 8c7e17867fac031d5f40477b8df654360f8ff8ce

Remove `nsenter` hack and the `--containerized="true"` flag

docker-images/kubelet-anywhere.sh
@@ -12,11 +12,9 @@ else
   args="--api-servers=http://${master}:8080"
 fi
 
-exec nsenter --target=1 --mount --wd=. -- \
-  ./hyperkube kubelet ${args} \
-    --docker-endpoint="unix:/weave.sock" \
-    --cluster-dns="10.16.0.3" \
-    --cluster-domain="kube.local" \
-    --containerized="true" \
-    --allow-privileged="true" \
-    --logtostderr="true"
+./hyperkube kubelet ${args} \
+  --docker-endpoint="unix:/weave.sock" \
+  --cluster-dns="10.16.0.3" \
+  --cluster-domain="kube.local" \
+  --allow-privileged="true" \
+  --logtostderr="true"

commit 319f3c8af73ba58083d5e683eb175e06104027d1

Put exec back in (broke in 8c7e17867fac031d5f40477b8df654360f8ff8ce)

docker-images/kubelet-anywhere.sh
@@ -12,9 +12,10 @@ else
   args="--api-servers=http://${master}:8080"
 fi
 
-./hyperkube kubelet ${args} \
+exec ./hyperkube kubelet ${args} \
   --docker-endpoint="unix:/weave.sock" \
   --cluster-dns="10.16.0.3" \
+  --resolv-conf="" \
   --cluster-domain="kube.local" \
   --allow-privileged="true" \
   --logtostderr="true"