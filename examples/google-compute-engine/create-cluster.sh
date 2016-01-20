8b1682c21f00080e6e1ac5f05182430f5530c26c
Ilya Dmitrichenko <errordeveloper@gmail.com>
2016-01-20 13:59:11 +0000

    Split internal and external firewall rules, make sure UDP port 6784 is open for Weaev Net FDP

--- examples/google-compute-engine/create-cluster.sh
+++ examples/google-compute-engine/create-cluster.sh
@@ -2,16 +2,14 @@
 p="--project weave-testing-1"
 z="--zone europe-west1-c"
 
-gcloud compute networks create $p \
-  'kube-net-1' \
+gcloud compute networks create $p 'kube-net-1' \
   --range '192.168.0.0/16'
-gcloud compute firewall-rules create $p \
-  'kube-net-1-fw' \
-  --network 'kube-net-1' \
-  --allow 'icmp,tcp:22,tcp:6783,udp:6783,tcp:4040'
-gcloud compute instances create $p $z \
-  $(seq -f 'kube-%g' 1 7) \
-  --image 'container-vm' \
-  --preemptible \
-  --network 'kube-net-1' \
-  --metadata-from-file 'startup-script=provision.sh'
+
+gcloud compute firewall-rules create $p 'kube-net-1-extfw' \
+  --network 'kube-net-1' --allow 'tcp:22,tcp:4040,tcp:30000-32767,udp:30000-32767'
+
+gcloud compute firewall-rules create $p 'kube-net-1-intfw' \
+  --network 'kube-net-1' --allow 'tcp:6783,udp:6783-6784' --source-ranges '192.168.0.0/16'
+
+gcloud compute instances create $p $z $(seq -f 'kube-%g' 1 7) \
+  --network 'kube-net-1' --image 'debian-8' --preemptible --metadata-from-file 'startup-script=provision.sh'
