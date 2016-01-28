835fda4812e3577ec8cc1b4e7e25aa3580facdea
Ilya Dmitrichenko <errordeveloper@gmail.com>
2016-01-28 13:30:31 +0000

    More TODO notes and some styling

--- examples/google-compute-engine/create-cluster.sh
+++ examples/google-compute-engine/create-cluster.sh
@@ -1,54 +1,77 @@
 #!/bin/bash -ex
 
-gcloud compute networks create 'kube-net-1' \
+gcloud compute networks create 'kube-net' \
   --range '192.168.0.0/16'
 
-gcloud compute firewall-rules create 'kube-net-1-extfw' \
-  --network 'kube-net-1' \
+gcloud compute firewall-rules create 'kube-extfw' \
+  --network 'kube-net' \
   --allow 'tcp:22,tcp:4040' \
-  --target-tags 'kube-ext'
+  --target-tags 'kube-ext' \
+  --description 'External access for SSH and Weave Scope user interface'
 
-gcloud compute firewall-rules create 'kube-net-1-intfw' \
-  --network 'kube-net-1' \
+gcloud compute firewall-rules create 'kube-intfw' \
+  --network 'kube-net' \
   --allow 'tcp:6783,udp:6783-6784' \
   --source-ranges '192.168.0.0/16' \
-  --target-tags 'kube-net-weave'
+  --target-tags 'kube-weave' \
+  --description 'Internal access for Weave Net ports'
 
-gcloud compute firewall-rules create 'kubernetes-minion-all' \
-  --network 'kube-net-1' \
+gcloud compute firewall-rules create 'kube-nodefw' \
+  --network 'kube-net' \
   --allow 'tcp,udp,icmp,esp,ah,sctp' \
   --source-ranges '192.168.0.0/16' \
-  --target-tags 'kubernetes-minion'
+  --target-tags 'kube-node' \
+  --description 'Internal access to all ports on the nodes'
 
 ## TODO: either figure out a sensible way to discover Weave Net peers
 ## and turn all the things into either one or more intance groups;
-## OR figure out how what's missing to make it all a flat set of
+## OR figure out what's missing to make it all a flat set of
 ## instances where cloud provider will still function as it is
+##
+## Peer discovery for Weave Net should be fairly simple, one can
+## either list all instance groups and find instances in those groups
+## OR look for given tags. Actually doing both would be best, as
+## we then can control whether there may be some instances that are
+## not on Weave Net, or if the `weave-net` tag spans multiple groups
+## which are independant of each other.
+##
+## However, it'd be hard to decide which of the instances in a group
+## should run `etcd1`, `etcd2` or `etcd3`. Hence the etcd nodes and
+## master could be part of an unmanaged instance group and thereby
+## retain predefined hostnames.
+
+gcloud compute instance-groups unmanaged delete -q 'kube-master-group'
 
 gcloud compute instances create $(seq -f 'kube-etcd-%g' 1 3) \
-  --network 'kube-net-1' \
-  --tags 'kube-net-weave,kube-ext' \
+  --network 'kube-net' \
+  --tags 'kube-weave,kube-ext' \
   --image 'debian-8' \
   --metadata-from-file 'startup-script=provision.sh' \
-  --boot-disk-type 'pd-standard' --boot-disk-size '20GB'
+  --boot-disk-type 'pd-standard' \
+  --boot-disk-size '20GB' \
+  --scopes 'compute-ro'
 
 gcloud compute instances create 'kube-master-0' \
-  --network 'kube-net-1' \
-  --tags 'kube-net-weave,kube-ext' \
+  --network 'kube-net' \
+  --tags 'kube-weave,kube-ext' \
   --image 'debian-8' \
   --metadata-from-file 'startup-script=provision.sh' \
-  --boot-disk-type 'pd-standard' --boot-disk-size '10GB' \
+  --boot-disk-type 'pd-standard' \
+  --boot-disk-size '10GB' \
   --can-ip-forward \
   --scopes 'storage-ro,compute-rw,monitoring,logging-write'
 
-gcloud compute instance-templates create 'kubernetes-minion-template' \
-  --network 'kube-net-1' \
-  --tags 'kube-net-weave,kube-ext,kubernetes-minion' \
+gcloud compute instance-templates create 'kube-node-template' \
+  --network 'kube-net' \
+  --tags 'kube-weave,kube-ext,kube-node' \
   --image 'debian-8' \
   --metadata-from-file 'startup-script=provision.sh' \
-  --boot-disk-type 'pd-standard' --boot-disk-size '30GB' \
+  --boot-disk-type 'pd-standard' \
+  --boot-disk-size '30GB' \
   --can-ip-forward \
   --scopes 'storage-ro,compute-rw,monitoring,logging-write'
 
-gcloud compute instance-groups managed create 'kubernetes-minion-group' \
-  --base-instance-name 'kubernetes-minion' --size 3 --template 'kubernetes-minion-template'
+gcloud compute instance-groups managed create 'kube-node-group' \
+  --template 'kube-node-template' \
+  --base-instance-name 'kube-node' \
+  --size 3
