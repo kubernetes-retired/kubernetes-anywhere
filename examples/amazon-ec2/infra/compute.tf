9bb611fb6f0ef1a3db671da7bd2393d45f6d50f1
Ilya Dmitrichenko <errordeveloper@gmail.com>
2016-01-29 09:47:37 +0000

    Figure out the rest of resource dependencies

--- examples/amazon-ec2/infra/compute.tf
+++ examples/amazon-ec2/infra/compute.tf
@@ -21,6 +21,114 @@ resource "aws_autoscaling_group" "kubernetes-minion-group" {
     }
 }
 
+resource "aws_instance" "kubernetes-node-1" {
+    ami                         = "ami-33566d03"
+    availability_zone           = "us-west-2a"
+    ebs_optimized               = false
+    instance_type               = "t2.micro"
+    monitoring                  = true
+    key_name                    = "terraform"
+    subnet_id                   = "${aws_subnet.kubernetes-subnet.id}"
+    vpc_security_group_ids      = ["${aws_security_group.kubernetes-node.id}"]
+    associate_public_ip_address = true
+    private_ip                  = "172.20.0.33"
+
+    root_block_device {
+        volume_type           = "gp2"
+        volume_size           = 32
+        iops                  = 96
+        delete_on_termination = true
+    }
+
+    tags {
+        "Role" = "kubernetes-minion"
+        "KubernetesCluster" = "kubernetes"
+        "Name" = "kubernetes-minion"
+        "aws:autoscaling:groupName" = "kubernetes-minion-group"
+    }
+}
+
+resource "aws_instance" "kubernetes-node-2" {
+    ami                         = "ami-33566d03"
+    availability_zone           = "us-west-2a"
+    ebs_optimized               = false
+    instance_type               = "t2.micro"
+    monitoring                  = true
+    key_name                    = "terraform"
+    subnet_id                   = "${aws_subnet.kubernetes-subnet.id}"
+    vpc_security_group_ids      = ["${aws_security_group.kubernetes-node.id}"]
+    associate_public_ip_address = true
+    private_ip                  = "172.20.0.34"
+
+    root_block_device {
+        volume_type           = "gp2"
+        volume_size           = 32
+        iops                  = 96
+        delete_on_termination = true
+    }
+
+    tags {
+        "aws:autoscaling:groupName" = "kubernetes-minion-group"
+        "KubernetesCluster" = "kubernetes"
+        "Name" = "kubernetes-minion"
+        "Role" = "kubernetes-minion"
+    }
+}
+
+resource "aws_instance" "kubernetes-node-3" {
+    ami                         = "ami-33566d03"
+    availability_zone           = "us-west-2a"
+    ebs_optimized               = false
+    instance_type               = "t2.micro"
+    monitoring                  = true
+    key_name                    = "terraform"
+    subnet_id                   = "${aws_subnet.kubernetes-subnet.id}"
+    vpc_security_group_ids      = ["${aws_security_group.kubernetes-node.id}"]
+    associate_public_ip_address = true
+    private_ip                  = "172.20.0.35"
+
+    root_block_device {
+        volume_type           = "gp2"
+        volume_size           = 32
+        iops                  = 96
+        delete_on_termination = true
+    }
+
+    tags {
+        "KubernetesCluster" = "kubernetes"
+        "Name" = "kubernetes-minion"
+        "aws:autoscaling:groupName" = "kubernetes-minion-group"
+        "Role" = "kubernetes-minion"
+    }
+}
+
+resource "aws_instance" "kubernetes-node-4" {
+    ami                         = "ami-33566d03"
+    availability_zone           = "us-west-2a"
+    ebs_optimized               = false
+    instance_type               = "t2.micro"
+    monitoring                  = true
+    key_name                    = "terraform"
+    subnet_id                   = "${aws_subnet.kubernetes-subnet.id}"
+    vpc_security_group_ids      = ["${aws_security_group.kubernetes-node.id}"]
+    associate_public_ip_address = true
+    private_ip                  = "172.20.0.36"
+
+    root_block_device {
+        volume_type           = "gp2"
+        volume_size           = 32
+        iops                  = 96
+        delete_on_termination = true
+    }
+
+    tags {
+        "aws:autoscaling:groupName" = "kubernetes-minion-group"
+        "Name" = "kubernetes-minion"
+        "Role" = "kubernetes-minion"
+        "KubernetesCluster" = "kubernetes"
+    }
+}
+
 resource "aws_instance" "kubernetes-master" {
     ami                         = "ami-33566d03"
     availability_zone           = "us-west-2a"
@@ -29,7 +137,7 @@ resource "aws_instance" "kubernetes-master" {
     monitoring                  = false
     key_name                    = "terraform"
     subnet_id                   = "${aws_subnet.kubernetes-subnet.id}"
-    vpc_security_group_ids      = ["sg-5f6e9638"]
+    vpc_security_group_ids      = ["${aws_security_group.kubernetes-master.id}"]
     associate_public_ip_address = true
     private_ip                  = "172.20.0.9"
     source_dest_check           = true
@@ -56,112 +164,3 @@ resource "aws_instance" "kubernetes-master" {
         "Role" = "kubernetes-master"
     }
 }
-
-resource "aws_instance" "kubernetes-minion" {
-    ami                         = "ami-33566d03"
-    availability_zone           = "us-west-2a"
-    ebs_optimized               = false
-    instance_type               = "t2.micro"
-    monitoring                  = true
-    key_name                    = "terraform"
-    subnet_id                   = "${aws_subnet.kubernetes-subnet.id}"
-    vpc_security_group_ids      = ["sg-5b6e963c"]
-    associate_public_ip_address = true
-    private_ip                  = "172.20.0.35"
-
-    root_block_device {
-        volume_type           = "gp2"
-        volume_size           = 32
-        iops                  = 96
-        delete_on_termination = true
-    }
-
-    tags {
-        "KubernetesCluster" = "kubernetes"
-        "Name" = "kubernetes-minion"
-        "aws:autoscaling:groupName" = "kubernetes-minion-group"
-        "Role" = "kubernetes-minion"
-    }
-}
-
-resource "aws_instance" "kubernetes-minion" {
-    ami                         = "ami-33566d03"
-    availability_zone           = "us-west-2a"
-    ebs_optimized               = false
-    instance_type               = "t2.micro"
-    monitoring                  = true
-    key_name                    = "terraform"
-    subnet_id                   = "${aws_subnet.kubernetes-subnet.id}"
-    vpc_security_group_ids      = ["sg-5b6e963c"]
-    associate_public_ip_address = true
-    private_ip                  = "172.20.0.33"
-
-    root_block_device {
-        volume_type           = "gp2"
-        volume_size           = 32
-        iops                  = 96
-        delete_on_termination = true
-    }
-
-    tags {
-        "Role" = "kubernetes-minion"
-        "KubernetesCluster" = "kubernetes"
-        "Name" = "kubernetes-minion"
-        "aws:autoscaling:groupName" = "kubernetes-minion-group"
-    }
-}
-
-resource "aws_instance" "kubernetes-minion" {
-    ami                         = "ami-33566d03"
-    availability_zone           = "us-west-2a"
-    ebs_optimized               = false
-    instance_type               = "t2.micro"
-    monitoring                  = true
-    key_name                    = "terraform"
-    subnet_id                   = "${aws_subnet.kubernetes-subnet.id}"
-    vpc_security_group_ids      = ["sg-5b6e963c"]
-    associate_public_ip_address = true
-    private_ip                  = "172.20.0.34"
-
-    root_block_device {
-        volume_type           = "gp2"
-        volume_size           = 32
-        iops                  = 96
-        delete_on_termination = true
-    }
-
-    tags {
-        "aws:autoscaling:groupName" = "kubernetes-minion-group"
-        "KubernetesCluster" = "kubernetes"
-        "Name" = "kubernetes-minion"
-        "Role" = "kubernetes-minion"
-    }
-}
-
-resource "aws_instance" "kubernetes-minion" {
-    ami                         = "ami-33566d03"
-    availability_zone           = "us-west-2a"
-    ebs_optimized               = false
-    instance_type               = "t2.micro"
-    monitoring                  = true
-    key_name                    = "terraform"
-    subnet_id                   = "${aws_subnet.kubernetes-subnet.id}"
-    vpc_security_group_ids      = ["sg-5b6e963c"]
-    associate_public_ip_address = true
-    private_ip                  = "172.20.0.36"
-
-    root_block_device {
-        volume_type           = "gp2"
-        volume_size           = 32
-        iops                  = 96
-        delete_on_termination = true
-    }
-
-    tags {
-        "aws:autoscaling:groupName" = "kubernetes-minion-group"
-        "Name" = "kubernetes-minion"
-        "Role" = "kubernetes-minion"
-        "KubernetesCluster" = "kubernetes"
-    }
-}
-
--- examples/amazon-ec2/infra/networking.tf
+++ examples/amazon-ec2/infra/networking.tf
@@ -10,29 +10,11 @@ resource "aws_vpc" "kubernetes-vpc" {
     }
 }
 
-resource "aws_security_group" "sg-796e961e-default" {
-    name        = "default"
-    description = "default VPC security group"
-    vpc_id      = "${aws_vpc.kubernetes-vpc.id}"
-
-    ingress {
-        from_port       = 0
-        to_port         = 0
-        protocol        = "-1"
-        self            = true
-    }
-
-
-    egress {
-        from_port       = 0
-        to_port         = 0
-        protocol        = "-1"
-        cidr_blocks     = ["0.0.0.0/0"]
-    }
-
+resource "aws_internet_gateway" "kubernetes-igw" {
+    vpc_id = "${aws_vpc.kubernetes-vpc.id}"
 }
 
-resource "aws_security_group" "sg-5f6e9638-kubernetes-master-kubernetes" {
+resource "aws_security_group" "kubernetes-master" {
     name        = "kubernetes-master-kubernetes"
     description = "Kubernetes security group applied to master nodes"
     vpc_id      = "${aws_vpc.kubernetes-vpc.id}"
@@ -71,8 +53,8 @@ resource "aws_security_group" "sg-5f6e9638-kubernetes-master-kubernetes" {
     }
 }
 
-resource "aws_security_group" "sg-5b6e963c-kubernetes-minion-kubernetes" {
-    name        = "kubernetes-minion-kubernetes"
+resource "aws_security_group" "kubernetes-node" {
+    name        = "kubernetes-node"
     description = "Kubernetes security group applied to minion nodes"
     vpc_id      = "${aws_vpc.kubernetes-vpc.id}"
 
@@ -103,7 +85,7 @@ resource "aws_security_group" "sg-5b6e963c-kubernetes-minion-kubernetes" {
     }
 }
 
-resource "aws_network_acl" "acl-9e9c82fb" {
+resource "aws_network_acl" "kubernetes-acl" {
     vpc_id     = "${aws_vpc.kubernetes-vpc.id}"
     subnet_ids = ["${aws_subnet.kubernetes-subnet.id}"]
 
@@ -140,49 +122,42 @@ resource "aws_subnet" "kubernetes-subnet" {
     }
 }
 
-resource "aws_route_table" "rtb-7446aa10" {
+resource "aws_route_table" "kubernetes-routes" {
     vpc_id     = "${aws_vpc.kubernetes-vpc.id}"
-
-    tags {
-    }
-}
-
-resource "aws_route_table" "rtb-7846aa1c" {
-    vpc_id     = "${aws_vpc.kubernetes-vpc.id}"
-
-    route {
-        cidr_block = "10.244.2.0/24"
-        instance_id = "i-358b6bed"
-        network_interface_id = "eni-b11e6efa"
-    }
 
     route {
         cidr_block = "10.244.0.0/24"
-        instance_id = "i-328b6bea"
-        network_interface_id = "eni-b31e6ef8"
-    }
-
-    route {
-        cidr_block = "10.246.0.0/24"
-        instance_id = "i-388868e0"
-        network_interface_id = "eni-e31d6da8"
-    }
-
-    route {
-        cidr_block = "10.244.3.0/24"
-        instance_id = "i-348b6bec"
-        network_interface_id = "eni-b21e6ef9"
+        instance_id = "${aws_instance.kubernetes-node-1.id}"
+        network_interface_id = "${aws_network_interface.kubernetes-node-1.id}"
     }
 
     route {
         cidr_block = "10.244.1.0/24"
-        instance_id = "i-378b6bef"
-        network_interface_id = "eni-bc1e6ef7"
+        instance_id = "${aws_instance.kubernetes-node-2.id}"
+        network_interface_id = "${aws_network_interface.kubernetes-node-2.id}"
+    }
+
+    route {
+        cidr_block = "10.244.2.0/24"
+        instance_id = "${aws_instance.kubernetes-node-3.id}"
+        network_interface_id = "${aws_network_interface.kubernetes-node-3.id}"
+    }
+
+    route {
+        cidr_block = "10.244.3.0/24"
+        instance_id = "${aws_instance.kubernetes-node-4.id}"
+        network_interface_id = "${aws_network_interface.kubernetes-node-4.id}"
+    }
+
+    route {
+        cidr_block = "10.246.0.0/24"
+        instance_id = "${aws_instance.kubernetes-master.id}"
+        network_interface_id = "${aws_network_interface.kubernetes-master.id}"
     }
 
     route {
         cidr_block = "0.0.0.0/0"
-        gateway_id = "igw-aa9cd8cf"
+        gateway_id = "${aws_internet_gateway.kubernetes-igw.id}"
     }
 
     tags {
@@ -190,63 +165,62 @@ resource "aws_route_table" "rtb-7846aa1c" {
     }
 }
 
-resource "aws_route_table_association" "rtb-7846aa1c-rtbassoc-12842276" {
-    route_table_id = "rtb-7846aa1c"
+resource "aws_route_table_association" "kubernetes-routes" {
+    route_table_id = "${aws_route_table.kubernetes-routes.id}"
     subnet_id = "${aws_subnet.kubernetes-subnet.id}"
 }
 
-resource "aws_network_interface" "eni-b11e6efa" {
-    subnet_id         = "${aws_subnet.kubernetes-subnet.id}"
-    private_ips       = ["172.20.0.34"]
-    security_groups   = ["sg-5b6e963c"]
-    source_dest_check = false
-    attachment {
-        instance     = "i-358b6bed"
-        device_index = 0
-    }
-}
-
-resource "aws_network_interface" "eni-b31e6ef8" {
-    subnet_id         = "${aws_subnet.kubernetes-subnet.id}"
-    private_ips       = ["172.20.0.35"]
-    security_groups   = ["sg-5b6e963c"]
-    source_dest_check = false
-    attachment {
-        instance     = "i-328b6bea"
-        device_index = 0
-    }
-}
-
-resource "aws_network_interface" "eni-e31d6da8" {
-    subnet_id         = "${aws_subnet.kubernetes-subnet.id}"
-    private_ips       = ["172.20.0.9"]
-    security_groups   = ["sg-5f6e9638"]
-    source_dest_check = true
-    attachment {
-        instance     = "i-388868e0"
-        device_index = 0
-    }
-}
-
-resource "aws_network_interface" "eni-bc1e6ef7" {
-    subnet_id         = "${aws_subnet.kubernetes-subnet.id}"
-    private_ips       = ["172.20.0.36"]
-    security_groups   = ["sg-5b6e963c"]
-    source_dest_check = false
-    attachment {
-        instance     = "i-378b6bef"
-        device_index = 0
-    }
-}
-
-resource "aws_network_interface" "eni-b21e6ef9" {
+resource "aws_network_interface" "kubernetes-node-1" {
     subnet_id         = "${aws_subnet.kubernetes-subnet.id}"
     private_ips       = ["172.20.0.33"]
-    security_groups   = ["sg-5b6e963c"]
+    security_groups   = ["${aws_security_group.kubernetes-node.id}"]
     source_dest_check = false
     attachment {
-        instance     = "i-348b6bec"
+        instance     = "${aws_instance.kubernetes-node-1.id}"
         device_index = 0
     }
 }
 
+resource "aws_network_interface" "kubernetes-node-2" {
+    subnet_id         = "${aws_subnet.kubernetes-subnet.id}"
+    private_ips       = ["172.20.0.34"]
+    security_groups   = ["${aws_security_group.kubernetes-node.id}"]
+    source_dest_check = false
+    attachment {
+        instance     = "${aws_instance.kubernetes-node-2.id}"
+        device_index = 0
+    }
+}
+
+resource "aws_network_interface" "kubernetes-node-3" {
+    subnet_id         = "${aws_subnet.kubernetes-subnet.id}"
+    private_ips       = ["172.20.0.35"]
+    security_groups   = ["${aws_security_group.kubernetes-node.id}"]
+    source_dest_check = false
+    attachment {
+        instance     = "${aws_instance.kubernetes-node-3.id}"
+        device_index = 0
+    }
+}
+
+resource "aws_network_interface" "kubernetes-node-4" {
+    subnet_id         = "${aws_subnet.kubernetes-subnet.id}"
+    private_ips       = ["172.20.0.36"]
+    security_groups   = ["${aws_security_group.kubernetes-node.id}"]
+    source_dest_check = false
+    attachment {
+        instance     = "${aws_instance.kubernetes-node-4.id}"
+        device_index = 0
+    }
+}
+
+resource "aws_network_interface" "kubernetes-master" {
+    subnet_id         = "${aws_subnet.kubernetes-subnet.id}"
+    private_ips       = ["172.20.0.9"]
+    security_groups   = ["${aws_security_group.kubernetes-master.id}"]
+    source_dest_check = true
+    attachment {
+        instance     = "${aws_instance.kubernetes-master.id}"
+        device_index = 0
+    }
+}
