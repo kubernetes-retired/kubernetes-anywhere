1706d10dc35e9d6452763e2983db45aa3015e3cc
Ilya Dmitrichenko <errordeveloper@gmail.com>
2016-02-20 11:33:58 +0000

    Try adding EFS, but turns out it is not GA yet

---
+++ examples/aws-ec2-terraform/storage.tf
@@ -0,0 +1,17 @@
+#resource "aws_efs_file_system" "kubernetes-efs" {
+#    reference_name = "kubernetes-efs"
+#
+#    tags {
+#        "KubernetesCluster" = "kubernetes"
+#        "Name"              = "kubernetes-efs"
+#    }
+#}
+#
+#resource "aws_efs_mount_target" "kubernetes-efs" {
+#    file_system_id  = "${aws_efs_file_system.kubernetes-efs.id}"
+#    subnet_id       = "${aws_subnet.kubernetes-subnet.id}"
+#    security_groups = [
+#        "${aws_security_group.kubernetes-master.id}",
+#        "${aws_security_group.kubernetes-node.id}",
+#    ]
+#}
