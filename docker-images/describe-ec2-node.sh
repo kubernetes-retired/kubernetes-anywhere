12a5cedd8d98666753df873e32c4d501b951bb5a
Ilya Dmitrichenko <errordeveloper@gmail.com>
2016-02-26 15:15:06 +0000

    Refactor to match `find-weave-peers-by-ec2-tag`

--- docker-images/describe-ec2-node.sh
+++ docker-images/describe-ec2-node.sh
@@ -5,12 +5,11 @@ set -o pipefail
 set -o nounset
 
 doc=$(curl --silent http://169.254.169.254/latest/dynamic/instance-identity/document)
-export AWS_DEFAULT_REGION=$(echo "${doc}" | jq -r .region)
+export AWS_DEFAULT_REGION=$(printf "${doc}" | jq -r .region)
 
 instance_description=$(
   aws ec2 describe-instances \
-    --instance-ids $(echo "${doc}" | jq -r .instanceId) \
-    --filters 'Name=tag:KubernetesCluster,Values=kubernetes'
+    --instance-ids $(printf "${doc}" | jq -r .instanceId)
 )
 
 instance_name_tag=$(
