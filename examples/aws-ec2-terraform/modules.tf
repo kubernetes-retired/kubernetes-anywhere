73a829e244d9f9a6c6cbeed58be220ea5fbe773b
Ilya Dmitrichenko <errordeveloper@gmail.com>
2016-02-29 17:12:26 +0000

    Fix distro nickname

--- examples/aws-ec2-terraform/modules.tf
+++ examples/aws-ec2-terraform/modules.tf
@@ -1,7 +1,7 @@
 module "ubuntu_ami" {
     source       = "github.com/terraform-community-modules/tf_aws_ubuntu_ami"
     region       = "${var.aws_region}"
-    distribution = "willy"
+    distribution = "wily"
     architecture = "amd64"
     virttype     = "hvm"
     storagetype  = "ebs-ssd"
