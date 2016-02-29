6327eb51ea821d533cb174857e36993140b06418
Ilya Dmitrichenko <errordeveloper@gmail.com>
2016-02-29 13:44:33 +0000

    Make it more usable as a module

--- examples/aws-ec2-terraform/variables.tf
+++ examples/aws-ec2-terraform/variables.tf
@@ -1,30 +1,15 @@
 variable "cluster" {
-   description = "Kubernetes cluster suffix"
-   default     = "a2"
-}
-
-variable "ec2_region" {
-   description = "EC2 region"
-   default     = "eu-west-1"
-}
-
-variable "ami" {
-   description = "AMI ID"
-   type        = "map"
-
-   default     = {
-       "eu-west-1"    = "ami-36289a45"
-       "eu-central-1" = "ami-30677d5c"
-   }
+   description = "Kubernetes cluster suffix (set this if you wish to deploy more then one cluster)"
+   default     = "anywhere"
 }
 
 variable "node_instance_type" {
-   description = "EC2 instance type for worker nodes"
+   description = "EC2 instance type for Kubernetes worker nodes"
    default     = "m3.xlarge"
 }
 
 variable "master_instance_type" {
-    description = "EC2 instance type for master node(s)"
+    description = "EC2 instance type for Kubernetes master node(s)"
     default     = "m3.xlarge"
 }
 
@@ -34,6 +19,14 @@ variable "etcd_instance_type" {
 }
 
 variable "ec2_key_name" {
-   description = "SSH key name to use for EC2 instances"
+   description = "SSH key name to use for all of EC2 instances"
    default     = "kubernetes-anywhere"
+}
+
+variable "aws_access_key" {}
+variable "aws_secret_key" {}
+
+variable "aws_region" {
+   description = "The EC2 region where to deploy Kubernetes cluster"
+   default     = "us-east-1"
 }
