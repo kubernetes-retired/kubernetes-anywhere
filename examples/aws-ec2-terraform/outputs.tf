e3cc0144f3e07b293e997361aa258db56b2bf729
Ilya Dmitrichenko <errordeveloper@gmail.com>
2016-03-01 10:56:18 +0000

    Build the URLs for ECRs

--- examples/aws-ec2-terraform/outputs.tf
+++ examples/aws-ec2-terraform/outputs.tf
@@ -11,9 +11,9 @@ output "kubernetes-main-sg-id" {
 }
 
 output "kubernetes-master-secure-config-repository" {
-    value = "${aws_ecr_repository.kubernetes-master-secure-config-repository.registry_id}/${aws_ecr_repository.kubernetes-master-secure-config-repository.name}"
+    value = "${aws_ecr_repository.kubernetes-master-secure-config-repository.registry_id}.dkr.ecr.${var.aws_region}.amazonaws.com/${aws_ecr_repository.kubernetes-master-secure-config-repository.name}"
 }
 
 output "kubernetes-node-secure-config-repository" {
-    value = "${aws_ecr_repository.kubernetes-node-secure-config-repository.registry_id}/${aws_ecr_repository.kubernetes-node-secure-config-repository.name}"
+    value = "${aws_ecr_repository.kubernetes-node-secure-config-repository.registry_id}.dkr.ecr.${var.aws_region}.amazonaws.com/${aws_ecr_repository.kubernetes-node-secure-config-repository.name}"
 }
