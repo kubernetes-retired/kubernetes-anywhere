810201d39554eb33d819108bbabf0e8f8475b639
Ilya Dmitrichenko <errordeveloper@gmail.com>
2016-03-01 22:01:21 +0000

    Remove spurious build step

--- docker-images/find-ecr-secure-config-images.sh
+++ docker-images/find-ecr-secure-config-images.sh
@@ -4,8 +4,6 @@ set -o errexit
 set -o pipefail
 set -o nounset
 
-setup-secure-cluster-config-volumes
-
 doc=$(curl --silent http://169.254.169.254/latest/dynamic/instance-identity/document)
 export AWS_DEFAULT_REGION=$(printf "${doc}" | jq -r .region)
 
