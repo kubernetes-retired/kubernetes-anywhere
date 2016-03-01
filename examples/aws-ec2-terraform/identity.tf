1af9ee5fc42776275b13bf37f666882744ccb1b2
Ilya Dmitrichenko <errordeveloper@gmail.com>
2016-03-01 09:26:06 +0000

    Fix JSON syntax

--- examples/aws-ec2-terraform/identity.tf
+++ examples/aws-ec2-terraform/identity.tf
@@ -55,7 +55,7 @@ resource "aws_iam_role_policy" "kubernetes-master" {
   "Version": "2012-10-17",
   "Statement": [{
       "Effect": "Allow",
-      "Action": [ "ec2:*", "elasticloadbalancing:*" "ecr:*" ],
+      "Action": [ "ec2:*", "elasticloadbalancing:*", "ecr:*" ],
       "Resource": "*"
    }]
 }
