3d119a2fdb4b8902e2215523459832ef2d9c0b60
Ilya Dmitrichenko <errordeveloper@gmail.com>
2016-03-01 10:53:36 +0000

    fixup! Output ECR repository name and regitry ID

--- examples/aws-ec2-terraform/outputs.tf
+++ examples/aws-ec2-terraform/outputs.tf
@@ -9,3 +9,11 @@ output "kubernetes-subnet-id" {
 output "kubernetes-main-sg-id" {
     value = "${aws_security_group.kubernetes-main-sg.id}"
 }
+
+output "kubernetes-master-secure-config-repository" {
+    value = "${aws_ecr_repository.kubernetes-master-secure-config-repository.registry_id}/${aws_ecr_repository.kubernetes-master-secure-config-repository.name}"
+}
+
+output "kubernetes-node-secure-config-repository" {
+    value = "${aws_ecr_repository.kubernetes-node-secure-config-repository.registry_id}/${aws_ecr_repository.kubernetes-node-secure-config-repository.name}"
+}
