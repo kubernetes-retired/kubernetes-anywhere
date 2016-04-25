be4f1e73c7647b50f12ebdff3338ef5920782cc3
Ilya Dmitrichenko <errordeveloper@gmail.com>
2016-04-25 15:10:36 +0100

    Cleanup stail firewall rules

--- examples/google-compute-engine/delete-cluster.sh
+++ examples/google-compute-engine/delete-cluster.sh
@@ -1,6 +1,7 @@
 #!/bin/bash -x
 
 etcd_instances=($(seq -s ' ' -f 'kube-etcd-%g' 1 3))
+dynamic_firewall_rules=($(gcloud compute firewall-rules list --regexp 'k8s-fw-.*' --uri))
 
 gcloud compute instances delete -q "${etcd_instances[@]}" 'kube-master-0'
 
@@ -10,7 +11,7 @@ gcloud compute instance-groups managed delete -q 'kube-node-group'
 
 gcloud compute instance-templates delete -q 'kube-node-template'
 
-gcloud compute firewall-rules delete -q 'kube-extfw' 'kube-intfw' 'kube-nodefw'
+gcloud compute firewall-rules delete -q 'kube-extfw' 'kube-intfw' 'kube-nodefw' "${dynamic_firewall_rules}"
 
 ## TODO: handle cleanup of dynamically allocated resources (forwarding rules, static IPs etc)
 
