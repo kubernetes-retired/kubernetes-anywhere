0fe768157beffc71b3787af92d69eede0c6b1add
Davanum Srinivas <davanum@gmail.com>
2017-11-19 14:45:34 -0500

    Add --discovery-token-unsafe-skip-ca-verification to prevent kubeadm join failure
    Since https://github.com/kubernetes/kubernetes/pull/55468 merged, kubeadm now requires the user to specify either the `--discovery-token-ca-cert-hash` flag or the `--discovery-token-unsafe-skip-ca-verification` flag

--- phase2/kubeadm/configure-vm-kubeadm-node.sh
+++ phase2/kubeadm/configure-vm-kubeadm-node.sh
@@ -1,4 +1,4 @@
 
 # This is not meant to run on its own, but extends phase2/kubeadm/configure-vm-kubeadm.sh
 
-kubeadm join --token "$KUBEADM_TOKEN" "$KUBEADM_MASTER_IP:443" --skip-preflight-checks
+kubeadm join --token "$KUBEADM_TOKEN" "$KUBEADM_MASTER_IP:443" --skip-preflight-checks --discovery-token-unsafe-skip-ca-verification
